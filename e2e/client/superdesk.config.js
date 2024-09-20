577086a27052da867bca2c2c8b1005d9686c225f
Nemanja Pavlovic <me@pavlovicnemanja.com>
2024-09-20 18:07:36 +0200

    feat(config): Added config for publisher

--- e2e/client/superdesk.config.js
+++ e2e/client/superdesk.config.js
@@ -2,8 +2,27 @@ module.exports = function(grunt) {
     return {
         importApps: [
             '../index',
+            'superdesk-publisher'
+        ],
+        apps: [
+            'superdesk-publisher'
         ],
         defaultRoute: '/workspace',
+        publisher: {
+            protocol: 'https',                /* http or https */
+            tenant: '',              /* tenant - semantically subdomain, '' is allowed */
+            domain: 'sp-publisher.superdesk.pro',           /* domain name for the publisher */
+            base: 'api/v2',                  /* api base path */
+
+            wsProtocol: 'wss',                /* ws or wss (websocket); if unspecified or '' defaults to 'wss' */
+            wsDomain: 'sp-publisher.superdesk.pro',  /* domain name (usually domain as above) */
+                                            /* e.g.: example.com, abc.example.com */
+                                            /* tenant, as above, is NOT used for websocket */
+            wsPath: '/ws',                    /* path to websocket root dir */
+            wsPort: '80',                   /* if not specified: defaults to 443 for wss, 80 for ws */
+            hideContentRoutesInPublishPane: false, /* hides routes of type "content" from select box in publish panes in monitoring view as well as in output control. If not specified: defaults to false */
+            hideCustomRoutesInPublishPane: false   /* hides routes of type "custom" from select box in publish panes in monitoring view as well as in output control. If not specified: defaults to false */
+        },
         features: {
             swimlane: {defaultNumberOfColumns: 4},
             editor3: true,
