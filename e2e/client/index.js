572c4082b1dfc5386a52b63f382f77ef6b2c7eb2
Nemanja Pavlovic <me@pavlovicnemanja.com>
2024-09-23 01:14:54 +0200

    feat(publisher): Included publisher extension

--- e2e/client/index.js
+++ e2e/client/index.js
@@ -2,7 +2,12 @@ import {startApp} from 'superdesk-core/scripts/index';
 
 setTimeout(() => {
     startApp(
-        [],
+        [
+            {
+                id: 'publisher-extension',
+                load: () => import('superdesk-publisher/client/publisher-extension'),
+            },
+        ],
         {}
     );
 });
